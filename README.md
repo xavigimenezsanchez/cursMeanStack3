5dd331fa3bee9402e5bf02e1ee64a211f4776875
xavigimenezsanchez <javiergimenezsanchez@gmail.com>
2015-02-01 19:42:57 +0000

    codi capítol dos

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# cursMeanStack3
+# Curs de l'stack mean
+## 3.- Integrant Node.js i Angular
+
+Per poder seguir aquest tema obre aquesta [presentació](htps://docs.google.com/presentation/d/1M61DZODHLgPhksakvLAC6KtcZkFAU8bqcv_G0urCsmc)
---
+++ server3.js
@@ -0,0 +1,32 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var Missatge = require("./models/missatge");
+
+var app = express();
+app.use(bodyParser.json());
+
+app.get("/api/missatges", function(req, res, next) {
+    Missatge.find(function(err, misstages) {
+        if (err) {
+            return next(err);
+        }
+        res.json(misstages);
+    });
+    
+});
+
+app.post("/api/missatges", function (req,res,next) {
+    var missatge = new Missatge({
+        username : req.body.username,
+        body: req.body.body
+    });
+    missatge.save(function(err, missatge) {
+        if (err) { return next(err) }
+        res.status(201).json(missatge);
+    });
+});
+
+app.listen(process.env.PORT, function() {
+    console.log('Server listening on', process.env.PORT);
+});
+
