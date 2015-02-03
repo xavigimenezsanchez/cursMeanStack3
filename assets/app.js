26e8ab863cf3f70859a904d0c4a0548b3b868678
xavigimenezsanchez <javiergimenezsanchez@gmail.com>
2015-02-03 15:31:41 +0000

    stage 4

---
+++ assets/app.js
@@ -0,0 +1,31 @@
+var app = angular.module('appLearn',[]);
+app.controller('MissatgesController',function($scope,MissatgesService) {
+        MissatgesService.fetch()
+        .success(function(missatges){
+            $scope.missatges = missatges;
+        })
+        .error(function(e){
+            console.log(e);
+        });
+
+        $scope.afegirMissatge = function() {
+            if ($scope.missatgeBody){
+                MissatgesService.create({
+                    username: "xavigimenez",
+                    body: $scope.missatgeBody
+                }).success(function(missatge) {
+                    $scope.missatges.unshift(missatge);
+                    $scope.missatgeBody = null;
+                });
+            }
+        };
+});
+        
+app.service("MissatgesService", function($http) {
+    this.fetch = function() {
+        return $http.get("/api/missatges");
+    };
+    this.create = function(missatge) {
+        return $http.post("/api/post");
+    };
+});
--- server.js
+++ server.js
@@ -1,18 +1,10 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
-var options = {
-    root: __dirname + "/layouts"
-};
 
 app.use(bodyParser.json());
 app.use("/api/missatges", require("./controllers/api/missatges"));
-
-
-app.get('/', function(req, res, next) {
-    
-    res.sendFile("missatges.html", options);
-});
+app.use("/",require("./controllers/static"));
 
 app.listen(process.env.PORT, function() {
     console.log('Server listening on', process.env.PORT);
